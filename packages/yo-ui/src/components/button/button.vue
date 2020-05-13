09d8d6b0d3b1a1281c031ed45139a7a3fbf99d42
collins401 <373345619@qq.com>
2020-05-13 23:20:50 +0800

    fix: test publish

--- packages/yo-ui/src/components/button/button.vue
+++ packages/yo-ui/src/components/button/button.vue
@@ -1,11 +1,50 @@
 <template>
-  <div class="yo-btn">
-    <slot>确定21</slot>
-  </div>
+  <button :class="`${b()} ${btnClass}`"
+    :type="type">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {
-  name: 'yo-button'
+  name: 'yo-button',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    radius: {
+      type: Boolean,
+      default: false,
+    },
+    plain: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    btnClass() {
+      return {
+        [this.type]: true,
+        radius: this.radius,
+        block: this.block,
+        [this.size]: true,
+        plain: this.reverse,
+        disabled: this.disabled,
+      }
+    },
+  }
 }
 </script>
--- packages/yo-ui/src/components/button/demo.vue
+++ packages/yo-ui/src/components/button/demo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1>button</h1>
-    <yo-button>企业的 ASD</yo-button>
+    <yo-button type="primary" block>企业的 ASD</yo-button>
   </div>
 </template>
